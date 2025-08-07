33a91c097aaae93059b283f93002d5d54b0e528c
AhrensOG <ahrensgabriel09@gmail.com>
2025-08-07 14:54:17 +0200

    Add functions to send contact form

--- components/public/home/Contact.tsx
+++ components/public/home/Contact.tsx
@@ -1,35 +1,37 @@
 "use client";
 
+import { sendContactFormFromHomePage } from "@/lib/api/contact";
 import { motion, useInView } from "framer-motion";
 import React, { useRef } from "react";
+import { toast } from "sonner";
+import { useForm } from "react-hook-form";
+
+type ContactFormData = {
+  name: string;
+  email: string;
+  phone: string;
+  message: string;
+};
 
 const Contact = () => {
   const ref = useRef(null);
   const isInView = useInView(ref, { amount: 0.3, once: true });
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const form = e.currentTarget;
-    const data = {
-      name: (form.elements.namedItem("name") as HTMLInputElement).value,
-      email: (form.elements.namedItem("email") as HTMLInputElement).value,
-      phone: (form.elements.namedItem("phone") as HTMLInputElement).value,
-      message: (form.elements.namedItem("message") as HTMLInputElement).value,
-    };
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<ContactFormData>();
+
+  const onSubmit = async (data: ContactFormData) => {
+    const toastId = toast.loading("Enviando mensaje...");
     try {
-      const res = await fetch("http://localhost:8000/contact", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data),
-      });
-      if (res.ok) {
-        alert("¡Mensaje enviado correctamente!");
-        form.reset();
-      } else {
-        alert("Ocurrió un error al enviar el mensaje. Intenta nuevamente.");
-      }
+      await sendContactFormFromHomePage(data);
+      toast.success("Mensaje enviado con éxito", { id: toastId });
+      reset();
     } catch {
-      alert("Ocurrió un error inesperado. Intenta nuevamente.");
+      toast.error("Error al enviar el mensaje", { id: toastId });
     }
   };
 
@@ -40,15 +42,13 @@ const Contact = () => {
       animate={isInView ? { opacity: 1, y: 0 } : {}}
       transition={{ duration: 0.8, ease: "easeOut" }}
       id="contacto"
-      className="py-24 "
-    >
+      className="py-24">
       <div className="container mx-auto px-6">
         <motion.h2
           initial={{ opacity: 0, y: -20 }}
           animate={isInView ? { opacity: 1, y: 0 } : {}}
           transition={{ duration: 0.6, ease: "easeOut" }}
-          className="text-4xl font-extrabold text-center text-custom-black-800 mb-12"
-        >
+          className="text-4xl font-extrabold text-center text-custom-black-800 mb-12">
           Contacta con nosotros
         </motion.h2>
 
@@ -56,85 +56,88 @@ const Contact = () => {
           initial={{ opacity: 0, scale: 0.95 }}
           animate={isInView ? { opacity: 1, scale: 1 } : {}}
           transition={{ duration: 0.8, ease: "easeOut" }}
-          className="max-w-xl mx-auto bg-custom-white-50 p-8 rounded-lg shadow-lg border border-custom-gray-300"
-        >
-          <form className="space-y-6" onSubmit={handleSubmit}>
+          className="max-w-xl mx-auto bg-custom-white-50 p-8 rounded-lg shadow-lg border border-custom-gray-300">
+          <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
               <motion.div
                 whileFocus={{ scale: 1.02 }}
-                transition={{ duration: 0.3 }}
-              >
+                transition={{ duration: 0.3 }}>
                 <label
                   htmlFor="name"
-                  className="block text-sm font-medium text-custom-black-800 mb-1"
-                >
+                  className="block text-sm font-medium text-custom-black-800 mb-1">
                   Nombre
                 </label>
                 <input
                   type="text"
                   id="name"
                   aria-label="Nombre"
-                  required
+                  {...register("name", { required: true })}
                   className="w-full px-4 py-2 border border-custom-gray-300 rounded-md focus:ring-2 focus:ring-custom-golden-600 focus:border-custom-golden-600 transition-all outline-none bg-custom-white-100 text-custom-black-800"
                 />
+                {errors.name && (
+                  <p className="text-red-500 text-xs mt-1">Campo obligatorio</p>
+                )}
               </motion.div>
 
               <motion.div
                 whileFocus={{ scale: 1.02 }}
-                transition={{ duration: 0.3 }}
-              >
+                transition={{ duration: 0.3 }}>
                 <label
                   htmlFor="email"
-                  className="block text-sm font-medium text-custom-black-800 mb-1"
-                >
+                  className="block text-sm font-medium text-custom-black-800 mb-1">
                   Email
                 </label>
                 <input
                   type="email"
                   id="email"
                   aria-label="Email"
-                  required
+                  {...register("email", { required: true })}
                   className="w-full px-4 py-2 border border-custom-gray-300 rounded-md focus:ring-2 focus:ring-custom-golden-600 focus:border-custom-golden-600 transition-all outline-none bg-custom-white-100 text-custom-black-800"
                 />
+                {errors.email && (
+                  <p className="text-red-500 text-xs mt-1">Campo obligatorio</p>
+                )}
               </motion.div>
             </div>
 
             <motion.div
               whileFocus={{ scale: 1.02 }}
-              transition={{ duration: 0.3 }}
-            >
+              transition={{ duration: 0.3 }}>
               <label
                 htmlFor="phone"
-                className="block text-sm font-medium text-custom-black-800 mb-1"
-              >
+                className="block text-sm font-medium text-custom-black-800 mb-1">
                 Teléfono
               </label>
               <input
-                type="tel"
+                type="number"
                 id="phone"
                 aria-label="Teléfono"
-                required
+                {...register("phone", { required: true })}
                 className="w-full px-4 py-2 border border-custom-gray-300 rounded-md focus:ring-2 focus:ring-custom-golden-600 focus:border-custom-golden-600 transition-all outline-none bg-custom-white-100 text-custom-black-800"
               />
+              {errors.phone && (
+                <p className="text-red-500 text-xs mt-1">Campo obligatorio</p>
+              )}
             </motion.div>
 
             <motion.div
               whileFocus={{ scale: 1.02 }}
-              transition={{ duration: 0.3 }}
-            >
+              transition={{ duration: 0.3 }}>
               <label
                 htmlFor="message"
-                className="block text-sm font-medium text-custom-black-800 mb-1"
-              >
+                className="block text-sm font-medium text-custom-black-800 mb-1">
                 Mensaje
               </label>
               <textarea
                 id="message"
                 rows={4}
                 aria-label="Mensaje"
-                required
+                {...register("message", { required: true })}
                 className="w-full px-4 py-2 border border-custom-gray-300 rounded-md focus:ring-2 focus:ring-custom-golden-600 focus:border-custom-golden-600 transition-all outline-none bg-custom-white-100 text-custom-black-800"
-              ></textarea>
+              />
+              {errors.message && (
+                <p className="text-red-500 text-xs mt-1">Campo obligatorio</p>
+              )}
             </motion.div>
 
             <motion.button
@@ -142,8 +145,7 @@ const Contact = () => {
               whileTap={{ scale: 0.95 }}
               transition={{ duration: 0.3 }}
               type="submit"
-              className="w-full bg-custom-golden-600 hover:bg-custom-golden-700 text-white font-medium py-3 px-4 rounded-lg transition-all outline-none shadow-md hover:shadow-lg"
-            >
+              className="w-full bg-custom-golden-600 hover:bg-custom-golden-700 text-white font-medium py-3 px-4 rounded-lg transition-all outline-none shadow-md hover:shadow-lg">
               Enviar mensaje
             </motion.button>
           </form>
